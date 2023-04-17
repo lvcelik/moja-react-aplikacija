c5d8e3ade42dbaecf0c0288f658552a214a7c654
Lovro Vcelik <lvcelik8@gmail.com>
2023-04-17 19:16:01 +0200

    Promjena 17.4.2023.

---
+++ src/componets/utilities/Oduzimanje.js
@@ -0,0 +1,3 @@
+export default function oduzmi (a, b){
+    return a - b;
+}
---
+++ src/componets/utilities/index.js
@@ -0,0 +1,3 @@
+export {default as Osoba} from './Person';
+export {imeAplikacije, num1 as broj1, pi, num2 as broj2, sum} from './Utility';
+export { default as oduzimanje } from './Oduzimanje';
