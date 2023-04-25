a9444b2d5c69bbfd76f7398e81fdbcba7d3e92ca
Lovro Vcelik <you@example.com>
2023-04-25 19:31:56 +0200

    Promjena 25.4.2023.

---
+++ src/componets/KorisnikFunckija.jsx
@@ -0,0 +1,10 @@
+export  default function KorisnikFunckija ({ime, godine}){
+    return (
+        
+            <p>Moje ime je {ime} i imam {godine} godina</p>
+
+        
+
+
+    );
+}
---
+++ src/componets/KorisnikKlasa.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class KorisnikKlasa extends React.Component{
+    render(){
+      const {ime, godine, onButtonClick} = this.props;
+      return (
+        <div>
+          <p>Moje ime je {ime} i imam {godine} godina</p>
+
+
+          <button onClick={onButtonClick}>
+            Klikni me!
+          </button>
+        </div>
+      );
+    }
+  }
