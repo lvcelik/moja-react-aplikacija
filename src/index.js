23801e0f2be9388d8f61f25394804f88eaf55fce
Lovro Vcelik <lvcelik8@gmail.com>
2023-05-08 18:16:55 +0200

    Promjena 8.5.2023. drugi put

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './componets/AppForme1';
 //import AppForme2 from './componets/AppForme2';
-import AppContext from './componets/AppContext';
+//import AppContext from './componets/AppContext';
+import AppHOC from './componets/AppHOC';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AppContext />);
+root.render(<AppHOC />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
