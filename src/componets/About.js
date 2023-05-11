7a0e292ed09ccc7fe09f5f40b9496e7a96e4c4a7
Lovro Vcelik <lvcelik8@gmail.com>
2023-05-11 17:34:40 +0200

    Promjena 11.5.2023.

---
+++ src/componets/About.js
@@ -0,0 +1,10 @@
+const About = () =>{
+    return (
+        <div>
+            <h2>About me</h2>
+            <p>Moje ime je .....</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/componets/Adresa.js
@@ -0,0 +1,10 @@
+const Adresa = () =>{
+    return (
+        <div>
+            <h2>Adresa</h2>
+            <p>Zagreb - Ilica 242, Maksimirska 58a</p>
+        </div>
+    );
+}
+
+export default Adresa;
